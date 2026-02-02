0a1f91ee061032f7624071b3431413ab3af05362
anlynaneudyrijo-ship-it <anlynaneudyrijo@gmail.com>
2026-02-01 23:26:51 -0400

    Actividad 1: Creacion de un programa basico
    en este programa simple llevo a cabo las instrucciones de crear lineas de codigo que pidan ciertas informaciones personales y mostrar la informacion dada en la misma consola.

---
+++ Programa Basico de Vilorio.py
@@ -0,0 +1,8 @@
+nombre = input("Cual es tu nombre? ")
+apellido = input("Cual es tu apellido? ")
+
+edad = int(input("Cuantos años tienes? "))
+altura = float(input("Cuanto mides? (en metros) "))
+
+print("\n----- Información Obtenida -----")
+print(f"Hola, mi nombre es {nombre} {apellido}, tengo {edad} años y mido {altura} metros.")
